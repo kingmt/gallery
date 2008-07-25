272c133d03e4b069fed10b81c98bdc11e6430ad9
Michael King <kingmt@gmail.com>
2008-07-25 07:18:51 -0500

    removed respond to blocks

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,33 +3,18 @@ class AlbumsController < ApplicationController
   # GET /albums.xml
   def index
     @albums = Album.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @albums }
-    end
   end
 
   # GET /albums/1
   # GET /albums/1.xml
   def show
     @album = Album.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @album }
-    end
   end
 
   # GET /albums/new
   # GET /albums/new.xml
   def new
     @album = Album.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @album }
-    end
   end
 
   # GET /albums/1/edit
@@ -41,16 +26,11 @@ class AlbumsController < ApplicationController
   # POST /albums.xml
   def create
     @album = Album.new(params[:album])
-
-    respond_to do |format|
-      if @album.save
-        flash[:notice] = 'Album was successfully created.'
-        format.html { redirect_to(@album) }
-        format.xml  { render :xml => @album, :status => :created, :location => @album }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @album.errors, :status => :unprocessable_entity }
-      end
+    if @album.save
+      flash[:notice] = 'Album was successfully created.'
+      redirect_to(@album) 
+    else
+      render :action => "new"
     end
   end
 
@@ -58,16 +38,11 @@ class AlbumsController < ApplicationController
   # PUT /albums/1.xml
   def update
     @album = Album.find(params[:id])
-
-    respond_to do |format|
-      if @album.update_attributes(params[:album])
-        flash[:notice] = 'Album was successfully updated.'
-        format.html { redirect_to(@album) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @album.errors, :status => :unprocessable_entity }
-      end
+    if @album.update_attributes(params[:album])
+      flash[:notice] = 'Album was successfully updated.'
+      redirect_to(@album) 
+    else
+       render :action => "edit" 
     end
   end
 
@@ -76,10 +51,6 @@ class AlbumsController < ApplicationController
   def destroy
     @album = Album.find(params[:id])
     @album.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(albums_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(albums_url)
   end
 end
