f68779c81829283546c072c7c8b5f810c735b384
Michael King <kingmt@gmail.com>
2008-07-25 11:43:08 -0500

    update for attachment_fu

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,12 @@
 class Picture < ActiveRecord::Base
   belongs_to :album
+  
+  has_attachment :content_type => :image,
+                 :storage => :file_system,
+                 :resize_to => '640x400',
+                 :max_size => 1.megabyte,
+                 :thumbnails => {:thumb => '100x100', :geometry => 'x50'}
+               
+  validates_as_attachment
+
 end
